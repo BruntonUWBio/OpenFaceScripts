39d7398a0186ab3c0543fa5ae49bebf9df4a4263
Jeffery Ni <jefferyni@gmail.com>
2018-03-30 11:34:19 -0700

    speech recognizer temp solution

--- speech/speech_recognizer.py
+++ speech/speech_recognizer.py
@@ -1,34 +1,33 @@
 import sys
 import os
 import shutil
-
+from collections import defaultdict
 import math
 
-sys.path.append('/home/gvelchuru')
+sys.path.append('/home/jeffery')
+# sys.path.append('/home/gvelchuru')
 from OpenFaceScripts.runners import CropAndOpenFace
 from OpenFaceScripts.runners.VidCropper import duration
 from OpenFaceScripts.scoring import AUScorer
 
+def make_mouth_presences(au_csv):
+    au_file_lines = open(au_csv).readlines()
+    line_one = au_file_lines[0].split(', ')
 
-def curr_lip_pos(mouth_points, moves, frame):
-    # ret_list = []
-    # try:
-    #     for point in mouth_points:
-    #         if 'x_{0}'.format(point) in moves[frame]:
-    #             ret_list.append((moves[frame]['x_{0}'.format(point)], moves[frame]['y_{0}'.format(point)]))
-    #     return ret_list
-    # except KeyError as e:
-    #     print('kurwa')
-    return [(moves[frame]['x_{0}'.format(point)], moves[frame]['y_{0}'.format(point)]) for point in mouth_points
-            if 'x_{0}'.format(point) in moves[frame]]
+    frame_loc = 0
+    au25_loc = -4
+    au26_loc = -3
 
+    d = defaultdict(list)
+    for i, line in enumerate(au_file_lines):
+        if i != 0:
+            items = line.split(', ')
+            if float(items[au25_loc]) == 1.00 or float(items[au26_loc]) == 1.00:
+                d[int(items[frame_loc]) - 1].append(True)
+            else:
+                d[int(items[frame_loc]) - 1].append(False)
 
-def aspect_ratio(curr_lip_coords):
-    min_x = min([x[0] for x in curr_lip_coords])
-    min_y = min([x[1] for x in curr_lip_coords])
-    max_x = max([x[0] for x in curr_lip_coords])
-    max_y = max([x[1] for x in curr_lip_coords])
-    return (max_y - min_y) / (max_x - min_x)
+    return d
 
 
 if __name__ == '__main__':
@@ -38,40 +37,20 @@ if __name__ == '__main__':
         out_parent_dir = os.path.dirname(out_file)
         working_directory = os.path.join(out_parent_dir, '{0}_speech_recognizer'.format(out_file))
         if not os.path.exists(working_directory):  # remove for faster debugging
-            # shutil.rmtree(working_directory)
             os.mkdir(working_directory)
             shutil.copy(vid, os.path.join(working_directory, 'inter_out.avi'))
             CropAndOpenFace.run_open_face(working_directory, True)
-        scorer = AUScorer.AUScorer(working_directory)
-        moves = scorer.x_y_dict
-        # presences = AUScorer.AUScorer(working_directory).presence_dict
+        presences = make_mouth_presences(os.path.join(working_directory, 'au.csv'))
         start_frame = 0
-        curr_string = None
-        mouth_points = list(range(49, 69))
         last_frame = None
         for frame in range(int(math.ceil(duration(vid) * 30))):
-            if frame not in moves:
+            if frame not in presences:
                 temp_string = "not recognized"
             else:
-                # if '25' in presences[frame] or '26' in presences[frame]:  # can set to specific vals as well
-                #     temp_string = "open mouth"
-                # else:
-                #     temp_string = "closed mouth"
-                if last_frame:
-                    curr_lip_coords = curr_lip_pos(mouth_points, moves, frame)
-                    curr_aspect = aspect_ratio(curr_lip_coords)
-                    prev_lip_coords = curr_lip_pos(mouth_points, moves, last_frame)
-                    prev_aspect = aspect_ratio(prev_lip_coords)
-                    tol = 10 ** -2  # Change for  performance
-                    if abs(prev_aspect - curr_aspect) >= tol:
-                        temp_string = "open mouth"
-                    else:
-                        temp_string = "closed mouth"
+                if presences[frame][0]:  # can set to specific vals as well
+                    temp_string = "open mouth"
                 else:
-                    temp_string = "no previous data"
-                last_frame = frame
-            if not curr_string or curr_string != temp_string:
-                if curr_string:
-                    out.write(str(start_frame) + '\t' + str(frame - 1) + '\t' + curr_string + '\n')
-                start_frame = frame
-                curr_string = temp_string
+                    temp_string = "closed mouth"
+            out.write(str(frame) + '\t' + temp_string + '\n')
+        shutil.rmtree(working_directory)
+
