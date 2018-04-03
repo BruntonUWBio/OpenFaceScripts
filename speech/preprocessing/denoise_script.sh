6fbb3b33cbc6e6e03133e59ce698ed77413a7941
Jeffery Ni <jefferyni@gmail.com>
2018-04-03 02:34:11 -0700

    changed temp file location

--- speech/preprocessing/denoise_script.sh
+++ speech/preprocessing/denoise_script.sh
@@ -8,11 +8,11 @@ mv $wavdir/*.wav $wavdir/noisy
 
 for file in $wavdir/noisy/*.wav
 do  
-  python3 $scriptdir/denoise_audio.py $file $scriptdir/audio.wav
-  sox -r 8000 $scriptdir/audio.wav -r 48000 $scriptdir/audio_up.raw
-  $scriptdir/rnnoise/examples/rnnoise_demo $scriptdir/audio_up.raw $scriptdir/audio_denoised.raw
-  sox -t s16 -c 1 -r 48000 $scriptdir/audio_denoised.raw -r 8000 $wavdir/$(basename $file)
-  rm $scriptdir/audio*
+  python3 $scriptdir/denoise_audio.py $file /tmp/audio.wav
+  sox -r 8000 /tmp/audio.wav -r 48000 /tmp/audio_up.raw
+  $scriptdir/rnnoise/examples/rnnoise_demo /tmp/audio_up.raw /tmp/audio_denoised.raw
+  sox -t s16 -c 1 -r 48000 /tmp/audio_denoised.raw -r 8000 $wavdir/$(basename $file)
+  rm /tmp/audio*
 done
 
 
