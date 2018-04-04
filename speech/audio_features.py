c64975c447dce5f4e230121c1d72b9402c8a81d3
Jeffery Ni <jefferyni@gmail.com>
2018-04-04 14:46:49 -0700

    fixed audio_features script

--- speech/audio_features.py
+++ speech/audio_features.py
@@ -2,11 +2,14 @@ import sys
 import numpy as np
 from python_speech_features import mfcc
 import scipy.io.wavfile as wav
+import _pickle as cPickle
+from os import path
 
 if __name__ == '__main__':
     audio = sys.argv[sys.argv.index('-a') + 1]
-    out_file = sys.argv[sys.argv.index('-t') + 1]
-    with open(out_file, 'w') as out:
-        signal, rate = wav.read(audio)
-        mfcc_feats = mfcc(signal, rate, winlen=1/30, nfft=512, numcep=26, winstep=1/30)
-        np.savetxt(out, mfcc_feats, delimiter='\t')
+    out_dir = sys.argv[sys.argv.index('-od') + 1]
+    filename = path.splitext(path.basename(audio))[0]
+
+    rate, signal = wav.read(audio)
+    mfcc_feats = mfcc(signal, samplerate=rate, winlen=1/30, nfft=512, numcep=26, winstep=1/30)
+    np.save(path.join(out_dir, filename), mfcc_feats)
