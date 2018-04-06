9536183a155545b6d3a996a04d1bedc49748f929
gvelchuru <gvelchuru@gmail.com>
2018-04-05 18:55:14 -0700

    adding context to open/close mouth

--- speech/speech_recognizer.py
+++ speech/speech_recognizer.py
@@ -13,9 +13,11 @@ from OpenFaceScripts.scoring import AUScorer
 if __name__ == '__main__':
     vid = sys.argv[sys.argv.index('-v') + 1]
     out_file = sys.argv[sys.argv.index('-t') + 1]
+    predicDic = {}
     with open(out_file, 'w') as out:
         out_parent_dir = os.path.dirname(out_file)
-        working_directory = os.path.join(out_parent_dir, '{0}_speech_recognizer'.format(out_file))
+        working_directory = os.path.join(
+            out_parent_dir, '{0}_speech_recognizer'.format(out_file))
 
         if not os.path.exists(working_directory):
             os.mkdir(working_directory)
@@ -26,7 +28,8 @@ if __name__ == '__main__':
         try:
             scorer = AUScorer.AUScorer(working_directory)
             presences = scorer.presence_dict
-            out.write('frame\t' + 'au25_c\t' + 'au26_c\t' + 'au25_r\t' + 'au26_r\t' + 'confidence\t' + 'prediction\n')
+            out.write('frame\t' + 'au25_c\t' + 'au26_c\t' +
+                      'au25_r\t' + 'au26_r\t' + 'confidence\t' + 'prediction\n')
 
             for frame in presences:
                 au25_c = 1 if '25' in presences[frame] else 0
@@ -36,11 +39,22 @@ if __name__ == '__main__':
                 confidence = presences[frame]['confidence']
 
                 if au25_c or au26_c:
-                        prediction = "open mouth"
+                    predicDic[frame] = "speaking"
                 else:
-                        prediction = "closed mouth"
+                    predicDic[frame] = "not speaking"
 
-                out.write('{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\n'.format(frame, au25_c, au26_c, au25_r, au26_r, confidence, prediction))
+                # out.write('{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\n'.format(
+                    # frame, au25_c, au26_c, au25_r, au26_r, confidence,
+                    # prediction))
+            predic_frames = sorted(list(predicDic.keys()))
+
+            for frame in predicDic:
+                prev_frame = predic_frames[predic_frames.index(frame) - 1]
+                if abs(prev_frame - frame) >= 5:
+                    prev_frame = None
+                next_frame = predic_frames[predic_frames.index(frame) + 1]
+
+
         except FileNotFoundError as e:
             print('{0} vid cannot be processed!'.format(vid))
         shutil.rmtree(working_directory)
